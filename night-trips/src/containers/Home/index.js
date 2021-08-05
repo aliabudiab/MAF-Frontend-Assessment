b3d8130e63692c3cdb63d37cdbb2dbf05c67d47c
Ali AbuDiab <aliabudiab112@gmail.com>
2021-08-06 01:51:00 +0300

    add social links

--- night-trips/src/containers/Home/index.js
+++ night-trips/src/containers/Home/index.js
@@ -1,4 +1,21 @@
+import { FiInstagram, FiTwitter, FiFacebook } from "react-icons/fi";
+
 const Index = () => {
+  const socialItems = [
+    {
+      path: "/",
+      icon: <FiTwitter size="22" />,
+    },
+    {
+      path: "/",
+      icon: <FiInstagram size="22" />,
+    },
+    {
+      path: "/",
+      icon: <FiFacebook size="22" />,
+    },
+  ];
+
   return (
     <section className="home-section" title="Home Section">
       <div className="content-wrapper">
@@ -10,6 +27,19 @@ const Index = () => {
           sed quis congue. Vel risus, arcu a viverra leo id pulvinar ultricies.
           Enim in in in pulvinar nulla sollicitudin. Ullamcorper.
         </p>
+      </div>
+
+      <div className="linear-gradient-div">
+        <div className="links-container">
+          <label className="scroll-label">SCROLL</label>
+          <ul className="social-links-container">
+            {socialItems.map((item, idx) => (
+              <li className="social-item" key={idx}>
+                {item.icon}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </section>
   );
